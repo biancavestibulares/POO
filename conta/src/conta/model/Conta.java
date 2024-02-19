69ea11cb26e4afe01f288f6d9e9d88821b46ebd8
Bianca Villalba de Andrade <157066592+biancavestibulares@users.noreply.github.com>
2024-02-19 11:38:20 -0300

    Add files via upload

---
+++ conta/src/conta/model/Conta.java
@@ -0,0 +1,46 @@
+package conta.model;
+
+public class Conta {
+	
+	//Adicionando os atributos da conta
+	//Atributos escolhidos foram titular, número da conta, tipo de conta, 
+	//saldo e status da conta
+	private String nomeTitular;
+	private String tipoConta;
+	private String statusConta;
+	private int numeroConta;
+	private float saldo;
+	
+	//Métodos Getters and Setters
+	public String getNomeTitular() {
+		return nomeTitular;
+	}
+	public void setNomeTitular(String nomeTitular) {
+		this.nomeTitular = nomeTitular;
+	}
+	public String getTipoConta() {
+		return tipoConta;
+	}
+	public void setTipoConta(String tipoConta) {
+		this.tipoConta = tipoConta;
+	}
+	public String getStatusConta() {
+		return statusConta;
+	}
+	public void setStatusConta(String statusConta) {
+		this.statusConta = statusConta;
+	}
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+	public float getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+	
+}
